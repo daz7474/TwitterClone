3ea1e083d3cb3ca94ba8c64db319b9b6acc89453
daz7474 <daz7474@gmail.com>
2024-05-30 16:32:43 +0100

    Auth workflow on client

---
+++ context/AuthProvider.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from "react";
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        login: (email, password) => {
+          // Communicate with backend and store token in SecureStore
+          setUser('Andre');
+        },
+        logout: () => {
+          setUser(null);
+        },
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
