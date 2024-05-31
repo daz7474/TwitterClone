5ccf9555b7c176f619aae0405494b0e3777fe269
daz7474 <daz7474@gmail.com>
2024-05-31 11:36:14 +0100

    Styles auth screens

--- screens/Auth/RegisterScreen.jsx
+++ screens/Auth/RegisterScreen.jsx
@@ -1,12 +1,78 @@
-import React from "react";
-import { View, Text, Button } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, StyleSheet, Button, Image, TouchableOpacity } from 'react-native';
 
 export default function RegisterScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>This is the register screen</Text>
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-        <Button onPress={() => navigation.navigate('Login Screen')} title="Go to Login Screen" />
+  function register(email, username, password, confirmPassword) {
+    Alert.alert('Register Logic here');
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={{ marginTop: 130, width: 260 }}>
+        <View style={{ alignItems: 'center' }}>
+          <Image style={styles.logo} source={require('../../assets/larydefault.png')} />
+        </View>
+        <View style={{ marginTop: 40 }}>
+          <TextInput
+            style={[ styles.inputBox, styles.mt4]}
+            onChangeText={setEmail}
+            value={email}
+            placeholder="Email"
+            placeholderTextColor='gray'
+            textContentType="emailAddress"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <TextInput
+            style={[ styles.inputBox, styles.mt4]}
+            onChangeText={setPassword}
+            value={password}
+            placeholder="Password"
+            placeholderTextColor='gray'
+            autoCapitalize="none"
+            secureTextEntry={true}
+          />
+        </View>
+
+        <TouchableOpacity
+          onPress={() => login(email, password)}
+          style={[styles.loginButton, styles.mt5]}
+        >
+          <Text style={styles.loginButtonText}>
+            Login
+          </Text>
+        </TouchableOpacity>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 12 }}>
+          <Text style={styles.registerText}>
+              Don't have an account?
+          </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Register Screen')}
+          >
+            <Text style={styles.registerTextLink}>
+              Register
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+            onPress={() => navigation.navigate('Login Screen')}
+          >
+            <Text style={styles.registerTextLink}>
+              Register
+            </Text>
+          </TouchableOpacity>
+      </View>
     </View>
   );
 }
